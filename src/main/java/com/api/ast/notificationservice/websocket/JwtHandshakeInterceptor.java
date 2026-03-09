1cd5ea11f77251c54dc769dac0fa88923fc76661
김문성 <k.ms@astkorea.net>
2026-03-09 16:23:39 +0900

    jwtHandshakeInterceptor 컴포넌트 등록으로 오류 수정

--- src/main/java/com/api/ast/notificationservice/websocket/JwtHandshakeInterceptor.java
+++ src/main/java/com/api/ast/notificationservice/websocket/JwtHandshakeInterceptor.java
@@ -3,12 +3,14 @@ package com.api.ast.notificationservice.websocket;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.HandshakeInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
+@Component
 public class JwtHandshakeInterceptor implements HandshakeInterceptor {
 
     @Override
